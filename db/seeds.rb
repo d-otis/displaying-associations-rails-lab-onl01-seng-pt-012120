06cab52795facbb7de77f3781f98eb74d089f0e5
d-otis <dan@dan-foley.com>
2020-05-28 16:05:18 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+nin = Artist.create!(name: "Nine Inch Nails")
+nin.songs.create(title: "The Perfect Drug")
+nin.songs.create(title: "Into the Void")
+
+pink = Artist.create!(name: "P!nk")
+pink.songs.create(title: "There You Go")
+pink.songs.create(title: "Glitter in the Air")
+
+mars_volta = Artist.create!(name: "The Mars Volta")
